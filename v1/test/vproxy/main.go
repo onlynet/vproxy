b40e8779d9280d3645bb3756ef9ff5b519f87215
456vv <123@456vv.com>
2016-09-14 12:17:53 +0800

    Add files via upload

--- v1/test/vproxy/main.go
+++ v1/test/vproxy/main.go
@@ -9,50 +9,75 @@ import (
     "flag"
     "fmt"
     "log"
+    "io"
+    "os"
+    "context"
 )
-var fshow = flag.Bool("show", false, "在控制台显示请求网址")
-var faddr = flag.String("addr", "0.0.0.0:8080", "代理服务器地")
+
+var flog = flag.String("log", "", "日志文件(默认在控制台显示日志)  (format \"./vproxy.txt\")")
+var fuser = flag.String("user", "", "用户名")
+var fpwd = flag.String("pwd", "", "密码")
+var flogLevel = flag.Int("logLevel", 0, "日志级别，0)不记录 1)客户端IP 2)认证 3)访问的Host地址 4)路径 5)请求 6)响应 7)错误")
+var faddr = flag.String("addr", "", "代理服务器地 (format \"0.0.0.0:8080\")")
 var fproxy = flag.String("proxy", "", "代理服务器的上级代理IP地址 (format \"11.22.33.44:8888\")")
-var fmaxIdleConnsPerHost = flag.Int("maxIdleConnsPerHost", 500, "保持空闲连接数量")
+var fmaxIdleConns = flag.Int("maxIdleConns", 500, "保持空闲连接(TCP)数量")
+var fmaxIdleConnsPerHost = flag.Int("maxIdleConnsPerHost", 500, "保持空闲连接(Host)数量")
 var fdisableKeepAlives = flag.Bool("disableKeepAlives", false, "禁止长连接 (default false)")
 var fdisableCompression = flag.Bool("disableCompression", false, "禁止传送数据时候进行压缩 (default false)")
 var ftlsHandshakeTimeout = flag.Int64("tlsHandshakeTimeout", 10000, "SSL握手超时，单位毫秒")
+var fidleConnTimeout = flag.Int64("idleConnTimeout", 0, "空闲连接超时时，单位毫秒")
 var fexpectContinueTimeout = flag.Int64("expectContinueTimeout", 1000, "http1.1过度到http2的等待超时，单位毫秒")
 var fresponseHeaderTimeout = flag.Int64("responseHeaderTimeout", 0, "读取服务器发来的文件标头超时，单位毫秒 (default 0)")
 var fdataBufioSize = flag.Int("dataBufioSize", 1024*10, "代理数据交换缓冲区大小，单位字节")
+var ftimeout = flag.Int64("timeout", 5000, "转发连接请求超时，单位毫秒")
 
 func main(){
     flag.Parse()
-    //if flag.NFlag() == 0 {
-    //    flag.PrintDefaults()
-    //    return
-    //}
+    if flag.NFlag() == 0 {
+        flag.PrintDefaults()
+        fmt.Println("\r\n命令行例子：vproxy -addr 0.0.0.0:8080")
+        return
+    }
+    var out io.Writer = os.Stdout
+    if *flog != "" {
+        file, err := os.OpenFile(*flog, os.O_CREATE | os.O_RDWR, 0777)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        out = file
+    }
 	p := &vproxy.Proxy{
+        Config      : &vproxy.Config{
+            DataBufioSize: *fdataBufioSize,
+            Timeout: time.Duration(*ftimeout) * time.Millisecond,
+        },
         Addr        : *faddr,
         Transport   : &http.Transport{
             DisableKeepAlives: *fdisableKeepAlives,
             DisableCompression: *fdisableCompression,
+            MaxIdleConns: *fmaxIdleConns,
             MaxIdleConnsPerHost: *fmaxIdleConnsPerHost,
+            IdleConnTimeout: time.Duration(*fidleConnTimeout) * time.Millisecond,
             ResponseHeaderTimeout: time.Duration(*fresponseHeaderTimeout) * time.Millisecond,
             TLSHandshakeTimeout:   time.Duration(*ftlsHandshakeTimeout) * time.Millisecond,
             ExpectContinueTimeout: time.Duration(*fexpectContinueTimeout) * time.Millisecond,
         },
-        Config      : &vproxy.Config{
-            DataBufioSize: *fdataBufioSize,
-        },
+        ErrorLogLevel: vproxy.LogLevel(*flogLevel),
     }
-    if *fshow {
-        p.Transport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error){
-            log.Println(req.URL)
-            return nil, nil
+    p.ErrorLog = log.New(out, "", log.LstdFlags)
+    if *fuser != "" {
+        p.Config.Auth = func(username, password string) bool {
+            return username == *fuser && password == *fpwd
         }
-    }
+     }
+
     if tr, ok := p.Transport.(*http.Transport); ok && *fproxy != "" {
         tr.Proxy = func(r *http.Request) (*url.URL, error){
             return r.URL, nil
         }
-        tr.Dial = func(network, addr string) (net.Conn, error){
-        	return net.Dial(network, *fproxy)
+        tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error){
+        	return new(net.Dialer).DialContext(ctx, network, *fproxy)
         }
     }
 
